f257268b6a28ffffae1178808e6d96df1d02d7df
nedba <nedbik@mail.ru>
2018-11-14 11:28:39 +0300

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,9 +1,7 @@
+const siteConfig = require('./projects/site')
+
 module.exports = {
     basePath: '/path/to/images',
     port: 1337,
-    small: {
-        width: 211,
-        height: 130,
-        quality: 100,
-    }
+    site: siteConfig
 }
